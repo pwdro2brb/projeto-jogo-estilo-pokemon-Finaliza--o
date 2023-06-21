24106a6f2507d6c241b5bb961ae36be94782499b
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-06-20 21:41:45 -0300

    Mudando algumas funções do código para funcionar as bordas do jogo Mudança necessária para a borda funcionar

--- index.js
+++ index.js
@@ -30,13 +30,13 @@ class Boundary {
 const boundaries = []
 
 const offset = {
-  x: -570,
-  y: -780
+  x: -500,
+  y: -800
 }
 
 collisionsMap.forEach((row, i) => {
   row.forEach((Symbol, j) => {
-    if (Symbol ===1044) {
+    if (Symbol === 1044) {
       boundaries.push(
        new Boundary({
          position: {
@@ -58,15 +58,44 @@ const playerImage = new Image();
 playerImage.src = './img/playerDown.png';
 
 class Sprite {
-  constructor({position, velocity, image}) {
+  constructor({position, velocity, image, frames = { max: 1} }) {
     this.position = position
     this.image = image
+    this.frames = frames
+
+    this.image.onload = () =>{
+      this.width = this.image.width / this.frames.max
+      this.height = this.image.height
+      console.log(this.width)
+      console.log(this.height)
+    }
   }
 
   draw() {
-    c.drawImage(this.image, this.position.x, this.position.y)
+    c.drawImage(
+      this.image,
+      0,
+      0,
+      this.image.width / this.frames.max,
+      this.image.height,
+      this.position.x,
+      this.position.y,
+      this.image.width / this.frames.max,
+      this.image.height
+    )
   }
 }
+
+const player = new Sprite({
+  position: {
+    x: canvas.width / 2 - 192 / 4 / 2,
+    y: canvas.height / 2 - 68 / 2
+  },
+  image: playerImage,
+  frames: {
+    max:4
+  }
+})
 
 const background = new Sprite({
   position: {
@@ -91,29 +120,45 @@ const keys = {
   }
 }
 
+const testBoundary = new Boundary({
+  position: {
+    x:400,
+    y:400
+  }
+})
+
+const movables = [background, testBoundary]
 function animate() {
   window.requestAnimationFrame(animate)
   background.draw()
-  boundaries.forEach(boundary => {
-    boundary.draw()
-  })
-  c.drawImage(
-    playerImage,
-    0,
-    0,
-    playerImage.width / 4,
-    playerImage.height,
-    canvas.width / 2 - playerImage.width / 2,
-    canvas.height / 2 - playerImage.height / 2,
-    playerImage.width / 4,
-    playerImage.height
-  )
+  //boundaries.forEach(boundary => {
+  //  boundary.draw()
+  //})
+  testBoundary.draw()
+  player.draw()
+  if (player.position.x + player.width >= testBoundary.position.x){
+    console.log('Bateu o corsa')
+  }
 
-  if (keys.w.pressed && lastKey === 'w') background.position.y += 3
-  else if (keys.a.pressed && lastKey === 'a') background.position.x += 3
-  else if (keys.s.pressed && lastKey === 's') background.position.y -= 3
-  else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3
-  
+  if (keys.w.pressed && lastKey === 'w') {
+    movables.forEach((movable) => {
+      movable.position.y += 3
+    })  
+  } else if (keys.a.pressed && lastKey === 'a') {
+    movables.forEach((movable) => {
+      movable.position.x += 3
+    })  
+  }
+  else if (keys.s.pressed && lastKey === 's') {
+    movables.forEach((movable) => {
+      movable.position.y -= 3
+    })  
+  }
+  else if (keys.d.pressed && lastKey === 'd') {
+    movables.forEach((movable) => {
+      movable.position.x -= 3
+    })  
+  }  
 }
 
 animate()
