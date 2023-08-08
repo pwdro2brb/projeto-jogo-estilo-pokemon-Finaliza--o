203631a1f618c2b122ee1484748f4b5832136d1f
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-08-07 22:22:24 -0300

    mostrando o tipo de ataque

--- data/attacks.js
+++ data/attacks.js
@@ -2,21 +2,25 @@ const attacks = {
   Ataque: {
     name:'Ataque', 
     damage: 5, 
-    type:'Normal' 
+    type:'Ataque corporal fraco',
+    color:'red' 
   }, 
   Bola_de_fogo: { 
     name:'Bola_de_fogo',
     damage: 20, 
-    type:'Fire' 
+    type:'Fogo',
+    color:'green'  
   },
   Impulso_Violento: { 
     name:'Impulso_Violento', 
     damage: 15, 
-    type:'StrongAttack' 
+    type:'Ataque corporal forte',
+    color:'red'  
   }, 
   Explosão_De_Ions: { 
     name:'Explosão_De_Ions', 
     damage: 30, 
-    type:'Explosion'
+    type:'Explosão',
+    color:'rgba' 
   } 
 }
